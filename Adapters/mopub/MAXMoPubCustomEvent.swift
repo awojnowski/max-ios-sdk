fecdd7aadec63f33ba3c7bff47de248160dbad7b
John Pena <john.pena@gmail.com>
2017-10-14 18:33:50 -0700

    Add MAX import to mopub custom event

--- Adapters/mopub/MAXMoPubCustomEvent.swift
+++ Adapters/mopub/MAXMoPubCustomEvent.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import MoPub
+import MAX
 
 @objc(MAXMoPubBannerCustomEvent)
 open class MAXMoPubBannerCustomEvent : MPBannerCustomEvent, MPBannerCustomEventDelegate, MAXAdViewDelegate {
