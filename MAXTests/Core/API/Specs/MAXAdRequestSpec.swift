7345bf6c85bf30b2d3475070236d4e873b255d01
Bryan Boyko <bryan.boyko@gmail.com>
2018-04-12 16:54:45 -0600

    Update version numbers to 1.1.0 (#124)

--- MAXTests/Core/API/Specs/MAXAdRequestSpec.swift
+++ MAXTests/Core/API/Specs/MAXAdRequestSpec.swift
@@ -142,7 +142,7 @@ class MAXAdRequestSpec: QuickSpec {
                 let reqDict = adRequest.dict
 
                 expect(reqDict["v"] as? String).to(equal("1"))
-                expect(reqDict["sdk_v"] as? String).to(equal("1.0.0"))
+                expect(reqDict["sdk_v"] as? String).to(equal("1.1.0"))
             }
 
             it("reports the app version") {
